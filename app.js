1532c7bb34625ad8f1a736c17605f14ec7a58304
Rafael Correia <rafaelnominato@gmail.com>
2015-07-14 17:41:23 -0300

    steam key separated from URL

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var request = require('request');
 var app = express();
 app.use(express.static(__dirname + '/app'))
 
+var key = 'B1D8105F367F2C4FE1ACA9C1F64EC504';
+
 app.get('/game/:user', function (req, res) {
 	var user = req.params.user;
-	request('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=B1D8105F367F2C4FE1ACA9C1F64EC504&vanityurl=' + user,
+	request('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + key + '&vanityurl=' + user,
 		function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var data = JSON.parse(body);
@@ -23,7 +25,7 @@ app.get('/game/:user', function (req, res) {
 });
 
 var getGame = function(steamid, res) {
-	request('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=B1D8105F367F2C4FE1ACA9C1F64EC504&steamid=' + steamid + '&format=json&include_appinfo=1',
+	request('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + key + '&steamid=' + steamid + '&format=json&include_appinfo=1',
 		function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
 		    var data = JSON.parse(body);
